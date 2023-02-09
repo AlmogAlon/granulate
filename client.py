f027a8b5b83dedd1e223a9a63285933451edb769
AlmogAlon <alon6781@gmail.com>
2023-02-09 10:29:19 +0200

    client: support chat room

--- client.py
+++ client.py
@@ -6,7 +6,6 @@ from typing import Optional, Dict
 import requests
 
 from common.settings import project_settings
-from routes.chat import Message
 from common import utils
 
 
@@ -14,18 +13,27 @@ server = project_settings().server
 CHAT_API = f"http://{server.addr}:{server.port}/chat"
 
 
-def send_message(message: str, username: str):
-    chat_message = Message(message=message, username=username)
+def send_message(message: str, username: str, room_name: str = None):
+    json_message = {
+        "username": username,
+        "message": message,
+    }
+    if room_name:
+        json_message["room_name"] = room_name
+
     try:
-        requests.post(CHAT_API, json=chat_message.to_dict())
+        requests.post(CHAT_API, json=json_message)
     except Exception as e:
         logging.error(f"Failed to send message: {e}")
         return
 
 
-def get_messages(username: str = None) -> Optional[Dict]:
+def get_messages(username: str, room_name: str = None) -> Optional[Dict]:
     try:
-        messages = requests.get(CHAT_API + f"?user={username}").json()
+        url = CHAT_API + f"?user={username}"
+        if room_name:
+            url += f"&room_name={room_name}"
+        messages = requests.get(url).json()
     except Exception as e:
         logging.error(f"Failed to get messages: {e}")
         return
@@ -37,10 +45,10 @@ def run(options):
 
     if options.message:
         message = input(f"Enter message (will be sent to {options.username}): ")
-        send_message(message, options.username)
+        send_message(message, options.username, options.room)
 
     while True:
-        messages = get_messages(options.username)
+        messages = get_messages(options.username, options.room)
         logging.info(messages)
         time.sleep(1)
 
@@ -50,6 +58,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-u", "--username", dest="username", help="username to get messages for", required=True
     )
+    parser.add_argument("-r", "--room", dest="room", help="room")
     parser.add_argument("--message", dest="message", action="store_true", help="send a message")
     args = parser.parse_args()
 
